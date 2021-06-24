5b8707936e95ce364baab15113ec4000d7f32aa8
lohithsrk <82163099+lohithsrk@users.noreply.github.com>
2021-06-24 10:20:21 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import HighlightIcon from '@material-ui/icons/Highlight';
 function Header() {
     return (
         <header>
-            <h1><HighlightIcon />Keeper</h1>
+            <h1><HighlightIcon />NoteIt</h1>
         </header>
     )
 }
